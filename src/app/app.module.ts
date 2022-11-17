aff716310f36b0814cbc340a909b613f8cfe74ca
HiepKaine <hiepwofl@gmail.com>
2022-11-17 17:15:49 +0700

    delete component abc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AbcComponent } from './abc/abc.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AbcComponent
   ],
   imports: [
     BrowserModule,
