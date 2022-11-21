a84af36d5b245ac790a3988c5a58f76548c5b6e9
HiepKaine <hiepwofl@gmail.com>
2022-11-21 17:39:04 +0700

    fix;

--- project-adngular/apps/backend/src/environments/environment.ts
+++ project-adngular/apps/backend/src/environments/environment.ts
@@ -1,3 +1,14 @@
 export const environment = {
   production: false,
+  appName: "Management-System",
+  appUrl: "http://localhost:3000",
+  appPrefix: "api",
+  appKey: "secret",
+  appVersion: '1.0',
+  dbConnection: 'mysql',
+  dbHost: 'localhost',
+  dbPort: 30000,
+  dbName: 'lms',
+  dbUser: 'root',
+  dbPass: 'secret',
 };
