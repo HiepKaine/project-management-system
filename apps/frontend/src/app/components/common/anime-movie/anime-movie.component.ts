7e3c292794a19c64c46706175a71b2dd7d272166
HiepKaine <hiepwofl@gmail.com>
2023-04-05 22:01:12 +0700

    fix anime-movie

--- apps/frontend/src/app/components/common/anime-movie/anime-movie.component.ts
+++ apps/frontend/src/app/components/common/anime-movie/anime-movie.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { AnimeMovie } from '@frontend/models/anime-movie.model';
 
 @Component({
@@ -6,7 +6,25 @@ import { AnimeMovie } from '@frontend/models/anime-movie.model';
   templateUrl: './anime-movie.component.html',
   styleUrls: ['./anime-movie.component.scss'],
 })
-export class AnimeMovieComponent {
+export class AnimeMovieComponent implements OnInit {
   @Input() item!: AnimeMovie;
+  @Input() type!: string;
   public isHover!: boolean;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  checkType(): string {
+    let result = ''
+    if (this.isHover){
+      if (this.type === 'film-random') {
+        result = 'hover-2';
+      } else {
+        result = 'hover';
+      }
+    }
+    return result;
+  }
 }
