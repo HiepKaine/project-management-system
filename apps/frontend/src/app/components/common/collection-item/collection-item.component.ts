28b550309e1262deaf1e82a278bb6ab38b042ced
HiepKaine <hiepwofl@gmail.com>
2023-04-05 23:30:44 +0700

    fix collection-item

--- apps/frontend/src/app/components/common/collection-item/collection-item.component.ts
+++ apps/frontend/src/app/components/common/collection-item/collection-item.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CollectionItemComponent implements OnInit {
   @Input() item!: any;
+  @Input() type!: string;
   public isHover!: boolean;
   arr = [1, 2, 3, 4, 5, 6, 7, 8];
   ngOnInit() {}
